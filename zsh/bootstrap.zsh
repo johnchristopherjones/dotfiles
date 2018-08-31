0778afbc25145a5a76168f30e29b2cef64c82143
John Christopher Jones <john.christopher@alumni.virginia.edu>
2018-08-31 02:30:02 -0400

    zsh: update boostrap paths

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,4 +1,12 @@
+# Add local (homebrew) bins
 PATH=/usr/local/sbin:${PATH}
 PATH=/usr/local/bin:${PATH}
-PATH=$(yarn global bin):${PATH}
+
+# Add yarn path
+PATH=$(yarn global bin):$(yarn bin):${PATH}
+
+# Add Visual Studio Code ("code")
+PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:${PATH}"
+
+# Add personal binaries
 PATH=~/.dotfiles/bin:${PATH}
